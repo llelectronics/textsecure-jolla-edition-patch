df2ad06ceac94e168b139d6f7e08018cc5e56665
mimi89999 <michel@lebihan.pl>
2016-02-24 15:01:55 +0100

    Remove location

--- src/org/thoughtcrime/securesms/components/AttachmentTypeSelector.java
+++ src/org/thoughtcrime/securesms/components/AttachmentTypeSelector.java
@@ -33,7 +33,6 @@ public class AttachmentTypeSelector extends PopupWindow {
   public static final int ADD_SOUND         = 3;
   public static final int ADD_CONTACT_INFO  = 4;
   public static final int TAKE_PHOTO        = 5;
-  public static final int ADD_LOCATION      = 6;
 
   private static final int ANIMATION_DURATION = 300;
 
@@ -44,7 +43,6 @@ public class AttachmentTypeSelector extends PopupWindow {
   private final @NonNull ImageView   videoButton;
   private final @NonNull ImageView   contactButton;
   private final @NonNull ImageView   cameraButton;
-  private final @NonNull ImageView   locationButton;
   private final @NonNull ImageView   closeButton;
 
   private @Nullable View                      currentAnchor;
@@ -56,26 +54,20 @@ public class AttachmentTypeSelector extends PopupWindow {
     LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     LinearLayout   layout   = (LinearLayout) inflater.inflate(R.layout.attachment_type_selector, null, true);
 
-    this.listener       = listener;
-    this.imageButton    = ViewUtil.findById(layout, R.id.gallery_button);
-    this.audioButton    = ViewUtil.findById(layout, R.id.audio_button);
-    this.videoButton    = ViewUtil.findById(layout, R.id.video_button);
-    this.contactButton  = ViewUtil.findById(layout, R.id.contact_button);
-    this.cameraButton   = ViewUtil.findById(layout, R.id.camera_button);
-    this.closeButton    = ViewUtil.findById(layout, R.id.close_button);
-    this.locationButton = ViewUtil.findById(layout, R.id.location_button);
+    this.listener      = listener;
+    this.imageButton   = ViewUtil.findById(layout, R.id.gallery_button);
+    this.audioButton   = ViewUtil.findById(layout, R.id.audio_button);
+    this.videoButton   = ViewUtil.findById(layout, R.id.video_button);
+    this.contactButton = ViewUtil.findById(layout, R.id.contact_button);
+    this.cameraButton  = ViewUtil.findById(layout, R.id.camera_button);
+    this.closeButton   = ViewUtil.findById(layout, R.id.close_button);
 
     this.imageButton.setOnClickListener(new PropagatingClickListener(ADD_IMAGE));
     this.audioButton.setOnClickListener(new PropagatingClickListener(ADD_SOUND));
     this.videoButton.setOnClickListener(new PropagatingClickListener(ADD_VIDEO));
     this.contactButton.setOnClickListener(new PropagatingClickListener(ADD_CONTACT_INFO));
     this.cameraButton.setOnClickListener(new PropagatingClickListener(TAKE_PHOTO));
-    this.locationButton.setOnClickListener(new PropagatingClickListener(ADD_LOCATION));
     this.closeButton.setOnClickListener(new CloseClickListener());
-
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-      ViewUtil.findById(layout, R.id.location_linear_layout).setVisibility(View.INVISIBLE);
-    }
 
     setContentView(layout);
     setWidth(LinearLayout.LayoutParams.MATCH_PARENT);
@@ -111,7 +103,6 @@ public class AttachmentTypeSelector extends PopupWindow {
       animateButtonIn(cameraButton, ANIMATION_DURATION / 2);
 
       animateButtonIn(audioButton, ANIMATION_DURATION / 3);
-      animateButtonIn(locationButton, ANIMATION_DURATION / 3);
       animateButtonIn(videoButton, ANIMATION_DURATION / 4);
       animateButtonIn(contactButton, 0);
       animateButtonIn(closeButton, 0);
